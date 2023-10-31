2a2258c17544b777019ed8abfe8dee25fe60a8d6
ericsondevmaster <ericson.devmaster@gmail.com>
2023-10-31 12:34:14 -0300

    Creating a session and executing db queries - SQLAlchemy ORM

--- integrationWithSQL/sqlAlchemyApplication.py
+++ integrationWithSQL/sqlAlchemyApplication.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import declarative_base, relationship, Session
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, inspect, select
 
 Base = declarative_base()
 
@@ -34,3 +34,49 @@ class Address(Base):
 
 print(User.__tablename__)
 print(Address.__tablename__)
+
+# conexão com o banco de dados
+engine = create_engine("sqlite://")
+
+# criando as classe como tabelas no banco de dados
+Base.metadata.create_all(engine)
+
+# investiga o esquema de banco de dados
+inspetor_engine = inspect(engine)
+print(inspetor_engine.has_table("user_account"))
+
+print(inspetor_engine.get_table_names())
+print(inspetor_engine.default_schema_name)
+
+with Session(engine) as session:
+    ericson = User(
+        name='ericson',
+        full_name='Ericson Alves',
+        address=[Address(email_address='ericson.devmaster@gmail.com')]
+    )
+
+    joao = User(
+        name='joao',
+        full_name='João Silva',
+        address=[Address(email_address='joaosilva@gmail.com'),
+                 Address(email_address='joaosilva@hotmail.com')]
+    )
+
+    maria = User(
+        name='maria',
+        full_name='Maria Pereira'
+    )
+
+# enviando para o DB (persistência de dados)
+session.add_all([ericson, joao, maria])
+session.commit()
+
+stmt = select(User).where(User.name.in_(['ericson', 'joao']))
+print("Recuperando usuários a partir de condição de filtragem")
+for user in session.scalars(stmt):
+    print(user)
+
+stmt_address = select(Address).where(Address.user_id.in_([2]))
+print("\nRecuperando os endereços de email de João")
+for addres in session.scalars(stmt_address):
+    print(addres)
